bea1129ddd9a5e7e910b7737ec33306ad0327406
oey192 <oeystanger@gmail.com>
2012-04-29 02:26:23 -0600

    Added unit test for Space, Air and Dirt

--- PlanetCraft.py
+++ PlanetCraft.py
@@ -4,13 +4,17 @@ import EntityManager
 
 
 def main():
-    print "Welcome to planet craft"
-    eMan = EntityManager
-    bMan = BlockManager
-    acore = bMan.Core("rwar")
-    aplan = eMan.Planet("test")
+    print "Welcome to PlanetCraft!"
+    em = EntityManager
+    bm = BlockManager
+    acore = bm.Core(21, 56, 30948)
+    aplan = em.Planet("test")
+    space1 = bm.Space(3, 5, 2)
+    dirt = bm.Dirt(5, 6, 8)
     print aplan
     print acore
+    print space1
+    print dirt
 
 if __name__ == '__main__':
     main()
