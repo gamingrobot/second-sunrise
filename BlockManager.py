abbdedc03cdd1c2c50090296183f2d37b6114a08
oey192 <oeystanger@gmail.com>
2012-05-05 01:06:26 -0600

    Added dynamic module loading for Blocks

--- BlockManager.py
+++ BlockManager.py
@@ -1,9 +1,23 @@
 #Block Manager
-import sys
-sys.path.insert(0, 'Blocks')
-from Air import *
+#import sys
+import pkgutil
+import os
+import imp
+#sys.path.insert(0, 'Blocks')
+'''from Air import *
 from Core import *
 from Dirt import *
 from Grass import *
 from Stone import *
-from Space import *
+from Space import *'''
+
+
+path = os.path.split(os.path.abspath(__file__))[0]
+path += "/Blocks/__init__.py"
+print path
+for module, name, isPkg in pkgutil.walk_packages(path):
+    if name.find("Blocks") == 0 and name != "Blocks":
+        mod_name = name[7:]
+        py_mod = imp.load_source(mod_name, path)    # Loads .py file
+        #py_mod = imp.load_compiled(mod_name,filename_path)     #loads .pyc file
+        print "imported " + name
---
+++ Blocks/__init__.py
@@ -0,0 +1,10 @@
+#import pkgutil
+#import os
+
+#print "I'm alive!"
+
+
+'''for module in pkgutil.iter_modules(os.path.abspath(__file__)):
+    if module[1] != "__init__.py":
+        #import module
+        print module[1]'''
