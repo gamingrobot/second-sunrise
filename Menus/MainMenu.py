2b489e783467f4305a9814888b86e2518edb2636
oey192 <oeystanger@gmail.com>
2012-06-13 00:36:40 -0600

    Added failed attempt at an options menu - forms appear to be broken in LibRocket for panda. cleaned up some menu thing and added transparent background

--- Menus/MainMenu.py
+++ Menus/MainMenu.py
@@ -1,22 +1,31 @@
 from Menu import *
+from OptionMenu import *
 
 
 class MainMenu(Menu):
-    #will probably need an instance of Controls in here in the same way
-    #OverlayMenu does
     def __init__(self, mainControl):
         print "hi"
         Menu.__init__(self, 'Menus/mainMenu.rml', 'mainMenu')
-        self.doc.Show()
+        #self.doc.Show()
+        self.show()
 
         self.controls = mainControl
+
+        self.optionMenu = OptionMenu(self)
 
         #get the play button and add a callback function for starting the game
         play = self.doc.GetElementById('play')
         play.AddEventListener('click', self.start, True)
 
+        option = self.doc.GetElementById('options')
+        option.AddEventListener('click', self.showOptions, True)
+
         quit = self.doc.GetElementById('quit')
         quit.AddEventListener('click', self.stop, True)
+
+    def showOptions(self):
+        self.doc.Hide()
+        self.optionMenu.show()
 
     def start(self):
         #will have a multiPlay function eventually for multiplayer
---
+++ Menus/OptionMenu.py
@@ -0,0 +1,27 @@
+from Menu import *
+
+
+class OptionMenu(Menu):
+    def __init__(self, returnMenu):
+        Menu.__init__(self, 'Menus/optionSheet.rml', 'optionMenu')
+
+        self.retMenu = returnMenu
+
+        #form = self.doc.GetElementById('options')
+        #form.AddEventListener('submit', self.saveOptions, True)
+
+        #remove this if/when forms work
+        temp = self.doc.GetElementById('temp')
+        temp.AddEventListener('click', self.saveOptions, True)
+
+        # read options here and set the appropriate elements
+        #to the appropriate values
+
+    # form processor - saves options
+    def saveOptions(self):
+        self.doc.Hide()
+        self.retMenu.show()
+
+    # reads options from however they're stored - called by __init__
+    def readOptions(self):
+        pass
