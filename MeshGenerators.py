64db488341bf3a84733c658e17d937987f8ed4bb
Morgan Creekmore <gamingrobot@gmail.com>
2012-09-12 11:47:50 -0600

    first attempt at dual contouring, really slow need to test on larger machine

--- MeshGenerators.py
+++ MeshGenerators.py
@@ -4,6 +4,10 @@ import Util
 import numpy as np
 import math
 from Blocks import *
+#DualContour test
+from simplexnoise import *
+import numpy.linalg as la
+import itertools as it
 
 import numpy.linalg as la
 #import scipy.optimize as opt
@@ -99,6 +103,7 @@ class DualContour:
         self.chunks = chunks
         self.radius = pradius
         self.noise = noise
+        self.vindex = {}
 
         self.center = np.array([16, 16, 16])
         self.radius = 5
@@ -112,10 +117,45 @@ class DualContour:
             for y in range(2)
             for z in range(2)]
 
+        #Edges of cube
+        self.cube_edges = [ 
+            [ k for (k,v) in enumerate(self.cube_verts) if v[i] == a and v[j] == b ]
+            for a in range(2)
+            for b in range(2)
+            for i in range(3) 
+            for j in range(3) if i != j ]
+
+    def estimate_hermite(self, v0, v1):
+        den, der = raw_noise_3d(v0[0]/15.0, v0[1]/15.0, v0[2]/15.0)
+        den2, der2 = raw_noise_3d(v1[0]/15.0, v1[1]/15.0, v1[2]/15.0)
+        t0 = (den + den) / 2
+        x0 = (1.-t0)*v0 + t0*v1
+        tempder = der + der2
+        dfx0 = [0,0,0]
+        dfx0[0], dfx0[1], dfx0[2] = tempder[0] / 2, tempder[1] / 2, tempder[2] / 2
+        return (x0, dfx0)
+
     def generateMesh(self):
+        dc_verts = []
         for x in xrange(0, self.size - 1):
             for y in xrange(0, self.size - 1):
                 for z in xrange(0, self.size - 1):
+                    o = np.array([x,y,z])
+                    #Get signs for cube
+                    #cube_signs = [ f(o+v)>0 for v in self.cube_verts ]
+                    #cube_signs = [True, True, True, True, True, True, True, True]
+                    #cube_signs = [False, False, False, False, False, False, False, False]
+
+
+                    h_data = []
+                    #for e in self.cube_edges:
+                    #    if cube_signs[e[0]] != cube_signs[e[1]]:
+                    #        h_data.append(self.estimate_hermite(o+self.cube_verts[e[0]], o+self.cube_verts[e[1]]))
+                    for e in self.cube_edges:
+                        h_data.append(self.estimate_hermite(o+self.cube_verts[e[0]], o+self.cube_verts[e[1]]))
+
+                    #print h_data
+
                     #Solve qef to get vertex
                     A = [n for p, n in h_data]
                     b = [np.dot(p, n) for p, n in h_data]
@@ -126,25 +166,25 @@ class DualContour:
                         continue
 
                     #Emit one vertex per every cube that crosses
-                    vindex[tuple(o)] = len(dc_verts)
+                    self.vindex[tuple(o)] = len(dc_verts)
+                    #print self.vindex
                     dc_verts.append(v)
+                    #print dc_verts
 
-                #Construct faces
-                dc_faces = []
-                for x, y, z in it.product(range(nc), range(nc), range(nc)):
-                    if not (x, y, z) in vindex:
-                        continue
+        #Construct faces
+        dc_faces = []
+        for x, y, z in it.product(range(self.size - 1), range(self.size - 1), range(self.size - 1)):
+            if not (x, y, z) in self.vindex:
+                continue
 
-                    #Emit one face per each edge that crosses
-                    o = np.array([x, y, z])
-                    for i in range(3):
-                        for j in range(i):
-                            if mask & 1:
-                                dc_faces.append((vindex[tuple(o)], vindex[tuple(o + self.dirs[i])], vindex[tuple(o + self.dirs[j])]))
-                                dc_faces.append((vindex[tuple(o + self.dirs[i] + self.dirs[j])], vindex[tuple(o + self.dirs[j])], vindex[tuple(o + self.dirs[i])]))
-                            else:
-                                dc_faces.append((vindex[tuple(o + self.dirs[j])], vindex[tuple(o + self.dirs[i])], vindex[tuple(o)]))
-                                dc_faces.append((vindex[tuple(o + self.dirs[i])], vindex[tuple(o + self.dirs[j])], vindex[tuple(o + self.dirs[i] + self.dirs[j])]))
+            #Emit one face per each edge that crosses
+            o = np.array([x, y, z])
+            for i in range(3):
+                for j in range(i):
+                    if tuple(o + self.self.dirs[i]) in self.vindex and tuple(o + self.self.dirs[j]) in self.vindex and tuple(o + self.dirs[i] + self.dirs[j]) in self.vindex:
+                        dc_faces.append( [self.vindex[tuple(o)], self.vindex[tuple(o+self.dirs[i])], self.vindex[tuple(o+self.dirs[j])]] )
+                        dc_faces.append( [self.vindex[tuple(o+self.dirs[i]+self.dirs[j])], self.vindex[tuple(o+self.dirs[j])], self.vindex[tuple(o+self.dirs[i])]] )
+
 
         dc_triangles = []
         for face in dc_faces:
