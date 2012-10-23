9cfc09dcf8775c3760ebc3e1ef69b0659ebc2ca2
Morgan Creekmore <gamingrobot@gmail.com>
2012-10-22 23:24:39 -0600

    added events and controls skeletons

---
+++ game/plugins/Core/controls/controls.py
@@ -0,0 +1,40 @@
+class Controls:
+    """Plugin for controlling the controls system"""
+    def __init__(self, manager, xml):
+        pass
+
+    def reload(self, manager, xml):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def destroy(self):
+        pass
+
+    def setFocusGame():
+        pass
+
+    def setFocusMenu():
+        pass
+
+    def registerKeyMenu(name, key, callback):
+        pass
+
+    def registerKeyGame(name, key, callback):
+        pass
+
+    def registerKeyAll(name, key, callback):
+        pass
+
+    def unRegisterKeyMenu(name, callback):
+        pass
+
+    def unRegisterKeyGame(name, callback):
+        pass
+
+    def unRegisterKeyAll(name, callback):
+        pass
