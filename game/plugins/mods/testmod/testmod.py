80d57f5b80bcc095b58593c2b5564f5f9de10da4
Morgan Creekmore <gamingrobot@gmail.com>
2012-11-27 17:55:36 -0700

    added code to testmod

---
+++ game/plugins/mods/testmod/testmod.py
@@ -0,0 +1,16 @@
+class TestMod:
+    def __init__(self, xml):
+        log.info("HELLO TEST MOD")
+        #you can set controls here via normal manager.get("controls").registerKeyAll(adlkjadkhl)
+
+    def reload(self, xml):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def destroy(self):
+        pass
